cfd3d0ca29c3cc87f511614d707d44258b3870fd
sigprecoodes <sig@precoodes.com>
2026-04-19 16:39:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const CONFIG = {
   GEOJSON_FILES: [
-    'data/Cuadrilla_3.geojson',
-    'data/Cuadrilla_8.geojson',
-    'data/Cuadrilla_9.geojson',
-    'data/cuadrilla_11.geojson'
+    '.data/Cuadrilla_3.geojson',
+    '.data/Cuadrilla_8.geojson',
+    '.data/Cuadrilla_9.geojson',
+    '.data/cuadrilla_11.geojson'
   ],
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzorYg7lSDUpYprv0wc4l_vdpHh7zO0uQGX96XZrmOoUwv6q_O-EEP_aVsrdeKY89TZ/exec',
   WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbzorYg7lSDUpYprv0wc4l_vdpHh7zO0uQGX96XZrmOoUwv6q_O-EEP_aVsrdeKY89TZ/exec'
